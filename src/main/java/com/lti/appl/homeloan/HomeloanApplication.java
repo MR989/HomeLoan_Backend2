366ea5449ad9ffa030120e1e02ac5371636eec0c
Mayank Ramani <mayankramani989@gmail.com>
2021-10-14 17:51:06 +0530

    2nd commit

--- src/main/java/com/lti/appl/homeloan/HomeloanApplication.java
+++ src/main/java/com/lti/appl/homeloan/HomeloanApplication.java
@@ -8,6 +8,8 @@ public class HomeloanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeloanApplication.class, args);
+		
+		System.out.println("hello");
 	}
 
 }
