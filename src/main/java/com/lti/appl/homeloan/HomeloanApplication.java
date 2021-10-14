b86c864b9e6886a6fa32797a4ee8d78fdcc382ba
Prajit Singh <prajitsingh2000@gmail.com>
2021-10-14 18:08:19 +0530

    3 cmmit

--- src/main/java/com/lti/appl/homeloan/HomeloanApplication.java
+++ src/main/java/com/lti/appl/homeloan/HomeloanApplication.java
@@ -9,7 +9,7 @@ public class HomeloanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomeloanApplication.class, args);
 		
-		System.out.println("hello");
+		System.out.println("hello Mayank");
 	}
 
 }
