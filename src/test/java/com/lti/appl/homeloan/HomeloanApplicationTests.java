6f2d6432f5087a9845c17f3f0da629edbd996427
Mayank Ramani <mayankramani989@gmail.com>
2021-10-14 17:41:45 +0530

    first commit

---
+++ src/test/java/com/lti/appl/homeloan/HomeloanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.appl.homeloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
